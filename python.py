c3fcabe6f0f8a8c3d8ff01c174efd6db4acbf026
haujul11 <haugejulian602@gmail.com>
2025-09-17 14:47:33 +0200

    lag en funksjon som viser alle

--- python.py
+++ python.py
@@ -1,11 +1,12 @@
-# Oppgave 1
-telefonbok = []
-
-person1 = {"navn": "Batman", "nummer": "BAT-2025"}
-person2 = {"navn": "Superman", "nummer": "KRY-9999"}
-
-telefonbok.append(person1)
-telefonbok.append(person2)
 
 
+# Oppgave 2
+def vis_alle():
+    if not telefonbok:
+        print("Telefonboka er tom.")
+    else:
+        print("\n--- Telefonbok ---")
+        for person in telefonbok:
+            print(f"{person['navn']}: {person['nummer']}")
+        print("------------------\n")
 
