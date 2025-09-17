ce9c33dacd04c5c366a00294b49312e29ae70ae5
haujul11 <haugejulian602@gmail.com>
2025-09-17 14:50:53 +0200

    test programmet

--- python.py
+++ python.py
@@ -1,23 +1,5 @@
 
 
-# Oppgave 5
-def hovedmeny():
-    print("Velkommen! (skriv 1/2/3/4 eller vis/legg/søk/avslutt)")
-    while True:
-        print("1. Vis alle\n2. Legg til ny\n3. Søk\n4. Avslutt")
-        valg = input("Ditt valg: ").strip().lower()
-
-        if valg in ("1", "vis", "v"):
-            vis_alle()
-        elif valg in ("2", "legg", "l", "legg til", "leggtil"):
-            legg_til()
-        elif valg in ("3", "søk", "sok", "s"):
-            sok()
-        elif valg in ("4", "avslutt", "stopp", "a"):
-            print("Programmet avsluttes.")
-            break
-        else:
-            print("Ugyldig valg, prøv igjen.\n")
-
-
+# Oppgave 6
+hovedmeny()
 
