0f3c6ebaa21401e1138c283d7a484bc5fc45ba5e
haujul11 <haugejulian602@gmail.com>
2025-09-17 14:50:04 +0200

    lag en hovedmeny

--- python.py
+++ python.py
@@ -1,12 +1,23 @@
 
 
-# Oppgave 4
-def sok():
-    navn = input("Hvem vil du søke etter? ").strip()
-    for person in telefonbok:
-        if person["navn"].lower() == navn.lower():
-            print(f"Fant: {person['navn']} - {person['nummer']}\n")
-            return
-    print("Person ikke funnet.\n")
+# Oppgave 5
+def hovedmeny():
+    print("Velkommen! (skriv 1/2/3/4 eller vis/legg/søk/avslutt)")
+    while True:
+        print("1. Vis alle\n2. Legg til ny\n3. Søk\n4. Avslutt")
+        valg = input("Ditt valg: ").strip().lower()
+
+        if valg in ("1", "vis", "v"):
+            vis_alle()
+        elif valg in ("2", "legg", "l", "legg til", "leggtil"):
+            legg_til()
+        elif valg in ("3", "søk", "sok", "s"):
+            sok()
+        elif valg in ("4", "avslutt", "stopp", "a"):
+            print("Programmet avsluttes.")
+            break
+        else:
+            print("Ugyldig valg, prøv igjen.\n")
+
 
 
