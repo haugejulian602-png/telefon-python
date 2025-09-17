f04cf5d77fe74fd5051e4d0f3ef4b2bc7a69872b
haujul11 <haugejulian602@gmail.com>
2025-09-17 14:46:09 +0200

    lage en liste med ordbøker

---
+++ python.py
@@ -0,0 +1,11 @@
+# Oppgave 1
+telefonbok = []
+
+person1 = {"navn": "Batman", "nummer": "BAT-2025"}
+person2 = {"navn": "Superman", "nummer": "KRY-9999"}
+
+telefonbok.append(person1)
+telefonbok.append(person2)
+
+
+
